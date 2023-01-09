d4e158e729d37e9e645b6e929fd5796f6e3834cc
dainiu <19307110036@fudan.edu.cn>
2023-01-09 19:36:51 +0800

    [solver] update package.

--- tests/test_shape_prop.py
+++ tests/test_shape_prop.py
@@ -3,7 +3,7 @@ import torch
 import torchvision.models as tm
 from colossalai.fx.tracer.experimental import symbolic_trace
 
-from metacol.passes.shape_prop import shape_prop_pass
+from siu.passes.shape_prop import shape_prop_pass
 
 tm_models = [
     tm.vgg11,
