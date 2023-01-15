c3a6e7f05aeee7c1a51874254e267763af3d927e
dainiu <19307110036@fudan.edu.cn>
2023-01-15 22:46:20 +0800

    [pre-commit] refactor

--- siu/_ops.py
+++ siu/_ops.py
@@ -1,11 +1,15 @@
 """Depreciated"""
 
-from siu.fx.passes.shape_prop import register_shape_impl
-from siu._subclasses import MetaTensor
 import torch
-
-from colossalai.auto_parallel.passes.runtime_apply_pass import runtime_apply, runtime_apply_for_iterable_object, runtime_comm_spec_apply
+from colossalai.auto_parallel.passes.runtime_apply_pass import (
+    runtime_apply,
+    runtime_apply_for_iterable_object,
+    runtime_comm_spec_apply,
+)
 from colossalai.tensor.comm_spec import CommSpec
+
+from siu._subclasses import MetaTensor
+from siu.fx.passes.shape_prop import register_shape_impl
 
 
 @register_shape_impl(runtime_apply)
@@ -17,7 +21,10 @@ def runtime_apply_impl(tensor, origin_dict, input_dict, node_index, user_node_in
 @register_shape_impl(runtime_apply_for_iterable_object)
 def runtime_apply_for_iterable_object_impl(tensor_list, origin_dict, input_dict, node_index, user_node_index):
     out_shape_list = [spec.get_sharded_shape_per_device() for spec in input_dict[node_index][user_node_index]]
-    return [MetaTensor(torch.empty(out_shape, device='meta', dtype=tensor.dtype), device=tensor.device) for out_shape, tensor in zip(out_shape_list, tensor_list)]
+    return [
+        MetaTensor(torch.empty(out_shape, device='meta', dtype=tensor.dtype), device=tensor.device)
+        for out_shape, tensor in zip(out_shape_list, tensor_list)
+    ]
 
 
 @register_shape_impl(runtime_comm_spec_apply)
