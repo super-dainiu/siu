fd9d2ed51b37d652066d13d50a70425928218ea1
dainiu <19307110036@fudan.edu.cn>
2023-01-31 11:18:48 +0800

    [siu] modify setup.

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import find_packages, setup
 
-setup(name='metacol',
-      packages=find_packages(exclude=(
-          'tests',
-          '*.egg-info',
-      )),
-      description='A unified meta cost model for Colossal-AI auto-parallel',
-      python_requires='>=3.6',
-      classifiers=[
-          'Programming Language :: Python :: 3',
-          'License :: OSI Approved :: Apache Software License',
-          'Environment :: GPU :: NVIDIA CUDA',
-          'Topic :: Scientific/Engineering :: Artificial Intelligence',
-          'Topic :: System :: Distributed Computing',
-      ],)
+setup(
+    name='siu',
+    packages=find_packages(exclude=(
+        'playground',
+        'tests',
+        '*.egg-info',
+    )),
+    version='0.0.1',
+    description='Solver integration utils for Colossal-AI',
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Environment :: GPU :: NVIDIA CUDA',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: System :: Distributed Computing',
+    ],
+)
